a2b255aa3eaf60b2d39942e2f670c588fbb43575
yangxing <yangxing@csii.com.cn>
2019-07-18 21:59:32 +0800

    1、相对复杂的生成器模式实现

---
+++ src/main/java/com/ancyxing/designpatterns/builderpatterns/usepatterns/compleximpl/Client.java
@@ -0,0 +1,14 @@
+package com.ancyxing.designpatterns.builderpatterns.usepatterns.compleximpl;
+
+/**
+ * @author yangxing
+ * @date 2019/7/18
+ */
+public class Client {
+    public static void main(String[] args) {
+        ConcreteBuilder builder = new ConcreteBuilder("001", 135L, 67890L);
+        InsuranceContract contract = builder.setPersonName("test").setOtherData("hello builder").build();
+        contract.doSomething();
+
+    }
+}
